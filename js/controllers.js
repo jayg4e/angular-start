3bcb3d7c02e47b1ba55586f351d9b503ddfa9dbb
unknown <akang2@gmail.com>
2015-06-17 14:36:32 -0700

    finished controller

--- js/controllers.js
+++ js/controllers.js
@@ -10,28 +10,36 @@ var myApp = angular.module('myApp', []);
 // 	}
 // );
 
-myApp.controller('MyController', 
-	function MyController($scope) {
-		$scope.artists = [
-		{
-			'name':  'mike bellingham',
-			'shortname': 'mikey',
-			'bio': 'mike sings for muse'
-		},
-		{
-			'name':  'chester pengington',
-			'shortname': 'chest',
-			'bio': 'chester sings for linkin park'	
-		},
-		{
-			'name':  'selena gomez',
-			'shortname': 'selly',
-			'bio': 'selena sings for selena'
-		},
-		{
-			'name':  'darren olson',
-			'shortname': 'd-ol',
-			'bio': 'darren sings for alabama shakes'
-		}
-		]
-});
+// myApp.controller('MyController', 
+// 	function MyController($scope) {
+// 		$scope.artists = [
+// 		{
+// 			'name':  'mike bellingham',
+// 			'shortname': 'mikey',
+// 			'bio': 'mike sings for muse'
+// 		},
+// 		{
+// 			'name':  'chester pengington',
+// 			'shortname': 'chest',
+// 			'bio': 'chester sings for linkin park'	
+// 		},
+// 		{
+// 			'name':  'selena gomez',
+// 			'shortname': 'selly',
+// 			'bio': 'selena sings for selena'
+// 		},
+// 		{
+// 			'name':  'darren olson',
+// 			'shortname': 'd-ol',
+// 			'bio': 'darren sings for alabama shakes'
+// 		}
+// 		]
+// });
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
+	$http.get('js/data.json').success(function(data) {
+		$scope.artists = data;
+	});
+}])
