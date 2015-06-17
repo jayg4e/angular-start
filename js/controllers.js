850f6b5f16e2ee4d559a13502c4e7751f6787c99
unknown <akang2@gmail.com>
2015-06-17 14:09:25 -0700

    added styling, updated controller to reflect new data

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,37 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('MyController',
-	function($scope) {
-		$scope.author = {
-			name: 'aleeex K',
-			title: 'tutor',
-			company : 'buildicus.com'
+// myApp.controller('MyController',
+// 	function($scope) {
+// 		$scope.author = {
+// 			name: 'aleeex K',
+// 			title: 'tutor',
+// 			company : 'buildicus.com'
+// 		}
+// 	}
+// );
+
+myApp.controller('MyController', 
+	function MyController($scope) {
+		$scope.artists = [
+		{
+			'name':  'mike bellingham',
+			'shortname': 'mikey',
+			'bio': 'mike sings for muse'
+		},
+		{
+			'name':  'chester pengington',
+			'shortname': 'chest',
+			'bio': 'chester sings for linkin park'	
+		},
+		{
+			'name':  'selena gomez',
+			'shortname': 'selly',
+			'bio': 'selena sings for selena'
+		},
+		{
+			'name':  'darren olson',
+			'shortname': 'd-ol',
+			'bio': 'darren sings for alabama shakes'
 		}
-	}
-);
+		]
+});
