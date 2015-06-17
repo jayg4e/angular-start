c94040fc1cdc4d43c8cc3c2747ee2d70a1578b6a
unknown <akang2@gmail.com>
2015-06-17 13:31:48 -0700

    first committ

---
+++ js/controllers.js
@@ -0,0 +1,11 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('MyController',
+	function() {
+		$scope.author = {
+			name: 'aleeex K',
+			title: 'tutor',
+			company : 'buildicus.com'
+		}
+	}
+);
